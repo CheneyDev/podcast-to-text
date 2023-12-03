28fe31ecbd262dbba749051ffe49b202aa86fec7
CheneyDev <cheneydev@gmail.com>
2023-12-03 15:23:04 +0800

    fix error: load resend api key

--- api.py
+++ api.py
@@ -7,15 +7,19 @@ app = FastAPI()
 
 load_dotenv()  # 加载.env文件中的环境变量
 
+
 @app.post("/transcribe/")
 async def transcribe(url: str):
     try:
-        api_key = os.getenv('OPENAI_API_KEY')
-        api_server = os.getenv('API_SERVER')
-        email_sender = os.getenv('SENDER_EMAIL')
-        email_receiver = os.getenv('RECEIVER_EMAIL')
+        api_key = os.getenv("OPENAI_API_KEY")
+        api_server = os.getenv("API_SERVER")
+        resend_api_key = os.getenv("RESEND_API_KEY")
+        email_sender = os.getenv("SENDER_EMAIL")
+        email_receiver = os.getenv("RECEIVER_EMAIL")
         print(api_server)
-        result = transcription.process_audio_and_send_email(url, api_key, api_server, email_sender, email_receiver)
+        result = transcription.process_audio_and_send_email(
+            url, api_key, api_server, resend_api_key, email_sender, email_receiver
+        )
         return {"transcription": result}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- transcription.py
+++ transcription.py
@@ -10,7 +10,10 @@ import resend
 MAX_FILE_SIZE_MB = 23
 BYTES_PER_MB = 1024 * 1024
 
-def send_audio_transcription_request(file_path, api_key, api_server, model="whisper-1", response_format="json"):
+
+def send_audio_transcription_request(
+    file_path, api_key, api_server, model="whisper-1", response_format="json"
+):
     # 发送音频转录请求的函数
     url = f"{api_server}/v1/audio/transcriptions"
     headers = {
@@ -25,7 +28,10 @@ def send_audio_transcription_request(file_path, api_key, api_server, model="whis
     response = requests.post(url, headers=headers, files=files)
     return response.json()
 
-def process_audio_and_send_email(url, api_key, api_server, email_sender, email_receiver):
+
+def process_audio_and_send_email(
+    url, api_key, api_server, resend_api_key, email_sender, email_receiver
+):
     # 1. 获取网页源代码并解析
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
@@ -39,7 +45,7 @@ def process_audio_and_send_email(url, api_key, api_server, email_sender, email_r
     parsed_url = urlparse(unquote(audio_url))
     extension = os.path.splitext(parsed_url.path)[1]
     file_name = f"{title}{extension}"
-    
+
     # 3. 下载音频文件
     response = requests.get(audio_url)
     audio_data = response.content
@@ -58,7 +64,9 @@ def process_audio_and_send_email(url, api_key, api_server, email_sender, email_r
 
     if file_size > MAX_FILE_SIZE_MB * BYTES_PER_MB:
         # 计算每个片段的持续时间（以毫秒为单位）
-        duration_per_segment_ms = len(audio) / (file_size / (MAX_FILE_SIZE_MB * BYTES_PER_MB))
+        duration_per_segment_ms = len(audio) / (
+            file_size / (MAX_FILE_SIZE_MB * BYTES_PER_MB)
+        )
 
         # 分割并保存音频片段
         start_ms = 0
@@ -66,14 +74,18 @@ def process_audio_and_send_email(url, api_key, api_server, email_sender, email_r
         while start_ms < len(audio):
             end_ms = min(start_ms + duration_per_segment_ms, len(audio))
             segment = audio[start_ms:end_ms]
-            segment_file_path = f"{os.path.splitext(temp_file_path)[0]}_part{part_num}.mp3"
+            segment_file_path = (
+                f"{os.path.splitext(temp_file_path)[0]}_part{part_num}.mp3"
+            )
             if os.path.exists(segment_file_path):  # 检查文件是否已存在
                 os.remove(segment_file_path)  # 如果存在，则删除
             segment.export(segment_file_path, format="mp3")
             start_ms = end_ms
             part_num += 1
             # 发送到API
-            api_response = send_audio_transcription_request(segment_file_path, api_key, api_server)
+            api_response = send_audio_transcription_request(
+                segment_file_path, api_key, api_server
+            )
             # 提取文本并添加到列表中
             transcription_json = api_response.json()
             if "text" in transcription_json:
@@ -82,7 +94,9 @@ def process_audio_and_send_email(url, api_key, api_server, email_sender, email_r
             os.remove(segment_file_path)
     else:
         # 转录整个音频文件
-        response_json = send_audio_transcription_request(temp_file_path, api_key, api_server)
+        response_json = send_audio_transcription_request(
+            temp_file_path, api_key, api_server
+        )
         if "text" in response_json:
             all_transcriptions.append(response_json["text"])
 
@@ -93,6 +107,7 @@ def process_audio_and_send_email(url, api_key, api_server, email_sender, email_r
     merged_transcription = "\n".join(all_transcriptions)
 
     # 8. 发送邮件
+    resend.api_key = resend_api_key
     sender_name = "Podcast to Text"
     params = {
         "from": sender_name + " <" + email_sender + ">",
@@ -104,6 +119,7 @@ def process_audio_and_send_email(url, api_key, api_server, email_sender, email_r
 
     return merged_transcription
 
+
 if __name__ == "__main__":
     # 加载环境变量
     load_dotenv()
@@ -111,7 +127,7 @@ if __name__ == "__main__":
     # 从环境变量中获取所需信息
     api_key = os.getenv("OPENAI_API_KEY")
     api_server = os.getenv("API_SERVER")
-    resend.api_key = os.getenv("RESEND_API_KEY")
+    resend_api_key = os.getenv("RESEND_API_KEY")
     email_sender = os.getenv("SENDER_EMAIL")
     email_receiver = os.getenv("RECEIVER_EMAIL")
 
@@ -119,5 +135,7 @@ if __name__ == "__main__":
     test_url = "https://www.xiaoyuzhoufm.com/episode/653944eb8f151344dc941a01"
 
     # 调用函数处理音频并发送邮件
-    result = process_audio_and_send_email(test_url, api_key, api_server, email_sender, email_receiver)
+    result = process_audio_and_send_email(
+        test_url, api_key, api_server, resend_api_key, email_sender, email_receiver
+    )
     print(result)
