5c605b5c863edc80f564f963c444424731cb6ed1
CheneyDev <cheneydev@gmail.com>
2023-12-03 16:07:01 +0800

    change para receive method to json body

--- api.py
+++ api.py
@@ -3,12 +3,16 @@ from fastapi import FastAPI, HTTPException
 import transcription
 from dotenv import load_dotenv
 from threading import Thread
+from pydantic import BaseModel
+
+class TranscriptionRequest(BaseModel):
+    url: str
 
 app = FastAPI()
 load_dotenv()  # 加载.env文件中的环境变量
 
 @app.post("/transcribe/")
-async def transcribe(url: str):
+async def transcribe(request: TranscriptionRequest):
     try:
         api_key = os.getenv("OPENAI_API_KEY")
         api_server = os.getenv("API_SERVER")
@@ -17,7 +21,7 @@ async def transcribe(url: str):
         email_receiver = os.getenv("RECEIVER_EMAIL")
 
         # 在线程中执行耗时的操作
-        thread = Thread(target=transcription.process_audio_and_send_email, args=(url, api_key, api_server, resend_api_key, email_sender, email_receiver))
+        thread = Thread(target=transcription.process_audio_and_send_email, args=(request.url, api_key, api_server, resend_api_key, email_sender, email_receiver))
         thread.start()
 
         return {"message": "已接受处理请求"}
